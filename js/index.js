957242ba45cffd82c3751c04cd4793f14c66b12e
rutu1518 <rutujamore1508@gmail.com>
2024-04-12 15:52:17 +0530

    js folder created

---
+++ js/index.js
@@ -0,0 +1,212 @@
+function clock() {
+    const hrs = document.getElementById('hrs');
+    const min = document.getElementById('min');
+    const sec = document.getElementById('sec');
+   
+
+    const date = new Date();
+    hrs.innerText = (date.getHours() < 10 ? "0" : "") + date.getHours() % 12 || 12;
+    min.innerText = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+    sec.innerText = (date.getSeconds() < 10 ? "0" : "") + date.getSeconds();
+  
+
+}
+setInterval(clock, 1000);
+clock();  
+
+
+function toggleheart(state){
+    const  heartElement = document.getElementById("Like-img");
+    if(state =='Full'){
+        heartElement.src = './images/index img/heart.png';
+    }         
+
+    else if (state == 'empty')
+    {
+        
+        heartElement.src = './images/index img/empty.png';
+    }         
+    
+}  
+
+function toggleheart2(state){
+    const  heartElement = document.getElementById("Like-img2");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart3(state){
+    const  heartElement = document.getElementById("Like-img3");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart4(state){
+    const  heartElement = document.getElementById("Like-img4");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart5(state){
+    const  heartElement = document.getElementById("Like-img5");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart6(state){
+    const  heartElement = document.getElementById("Like-img6");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart7(state){
+    const  heartElement = document.getElementById("Like-img7");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart8(state){
+    const  heartElement = document.getElementById("Like-img8");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart9(state){
+    const  heartElement = document.getElementById("Like-img9");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart10(state){
+    const  heartElement = document.getElementById("Like-img10");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart11(state){
+    const  heartElement = document.getElementById("Like-img11");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart12(state){
+    const  heartElement = document.getElementById("Like-img12");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart13(state){
+    const  heartElement = document.getElementById("Like-img13");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart14(state){
+    const  heartElement = document.getElementById("Like-img14");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
