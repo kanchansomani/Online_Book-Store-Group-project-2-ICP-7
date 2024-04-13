473e866784c6cff9e22d8a2f45942123c45f8a03
rutu1518 <rutujamore1508@gmail.com>
2024-04-13 14:19:30 +0530

    like symbol added in non fiction and new arrival page

---
+++ js/Coffe-table-book.js
@@ -0,0 +1,141 @@
+function toggleheart(state){
+    const  heartElement = document.getElementById("Like-img");
+    if(state =='Full'){
+        heartElement.src = './images/index img/heart.png';
+    }         
+
+    else if (state == 'empty')
+    {
+        
+        heartElement.src = './images/index img/empty.png';
+    }         
+    
+} 
+
+
+
+function toggleheart2(state){
+    const  heartElement = document.getElementById("Like-img2");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart3(state){
+    const  heartElement = document.getElementById("Like-img3");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart4(state){
+    const  heartElement = document.getElementById("Like-img4");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart5(state){
+    const  heartElement = document.getElementById("Like-img5");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart6(state){
+    const  heartElement = document.getElementById("Like-img6");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart7(state){
+    const  heartElement = document.getElementById("Like-img7");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart8(state){
+    const  heartElement = document.getElementById("Like-img8");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart9(state){
+    const  heartElement = document.getElementById("Like-img9");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
+
+function toggleheart10(state){
+    const  heartElement = document.getElementById("Like-img10");
+    if(state =='Empty'){
+        heartElement.src = './images/index img/empty.png';
+    }         
+
+    else if (state == 'Full')
+    {
+        
+        heartElement.src = './images/index img/heart.png';
+    }         
+    
+}  
